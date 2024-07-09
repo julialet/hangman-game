edb4c0dfef90e7a5ff81286c8b481b00789e29f1
Julia <juliahille2004@gmail.com>
2024-07-09 10:36:28 -0300

    final commit

---
+++ scripts/script.js
@@ -0,0 +1,68 @@
+const keyboardDiv = document.querySelector(".keyboard");
+const wordDisplay = document.querySelector(".word-display");
+const guessesText = document.querySelector(".guesses b");
+const hangmanImage = document.querySelector(".hangman-box img");
+const gameModal = document.querySelector(".game-modal");
+const playAgainBtn = document.querySelector(".play-again");
+
+let currentWord, correctLetters, wrongGuessCount;
+const maxGuesses = 6;
+
+const resetGame = () => {
+    correctLetters = [];
+    wrongGuessCount = 0;
+    hangmanImage.src = `images/hangman-${wrongGuessCount}.svg`;
+    guessesText.innerText = `${wrongGuessCount} / ${maxGuesses}`;
+    keyboardDiv.querySelectorAll("button").forEach(btn => btn.disabled = false);
+    wordDisplay.innerHTML = currentWord.split("").map(() => `<li class="letter"></li>`).join("");
+    gameModal.classList.remove("show");
+}
+
+const getRandomWord = () => {
+    const { word, hint } = wordList[Math.floor(Math.random() * wordList.length)];
+    currentWord = word;
+    document.querySelector(".hint b").innerText = hint;
+    resetGame();
+}
+
+const gameOver = (isVictory) => {
+    setTimeout(()=> {
+        const modalText = isVictory ? `Você achou a palavra:` : `A palavra correta era:`;
+        gameModal.querySelector("img").src = `images/${isVictory ? 'victory' : 'lost'}.png`;
+        gameModal.querySelector("h4").innerText = `${isVictory ? 'Eba!' : 'Fim de jogo!'}`;
+        gameModal.querySelector("p").innerHTML = `${modalText} <b>${currentWord}</b>`;
+        gameModal.classList.add("show");
+
+    }, 300);
+}
+
+const initGame = (button, clickedLetter) => {
+    if (currentWord.includes(clickedLetter)) {
+        [...currentWord].forEach((letter, index) => {
+            if (letter === clickedLetter) {
+                correctLetters.push(letter);
+                wordDisplay.querySelectorAll("li")[index].innerText = letter;
+                wordDisplay.querySelectorAll("li")[index].classList.add("guessed");
+            }
+        });
+    } else {
+        wrongGuessCount++;
+        hangmanImage.src = `images/hangman-${wrongGuessCount}.svg`;
+    }
+    button.disabled = true;
+    guessesText.innerText = `${wrongGuessCount} / ${maxGuesses}`;
+
+    if(wrongGuessCount == maxGuesses) return gameOver(false);
+    if(correctLetters.length == currentWord.length) return gameOver(true);
+}
+
+for (let i = 97; i <= 122; i++) {
+    const button = document.createElement("button");
+    button.innerText = String.fromCharCode(i);
+    keyboardDiv.appendChild(button);
+    button.addEventListener("click", e => initGame(e.target, String.fromCharCode(i)));
+}
+
+getRandomWord();
+playAgainBtn.addEventListener("click", getRandomWord);
+
---
+++ scripts/word-list.js
@@ -0,0 +1,27 @@
+const wordList = [
+    {
+        word: "anamnese",
+        hint: "entrevista realizada com o paciente a fim de estabelecer uma avaliação e diagnóstico"
+    },
+    {
+        word: "freud",
+        hint: "o criador da psicanálise e a personalidade mais influente da história no campo da psicologia"
+    },
+    {
+        word: "esportiva",
+        hint: "uma das áreas da psicologia, trabalha em conjunto com atletas"
+    },
+    {
+        word: "cognicao",
+        hint: "processo mental que envolve percepção, memória, pensamento e compreensão"
+    },
+    {
+        word: "terapia",
+        hint: "tratamento psicológico que visa resolver conflitos emocionais e promover o bem-estar"
+    },
+    {
+        word: "transtorno",
+        hint: "condição psicológica que causa dificuldades no funcionamento normal da vida"
+    },
+    
+];
